98ccc5fee990a22c4f35c0a9f35e79e699f64be2
willieLjohnson <liwa.johnson@gmail.com>
2020-05-13 20:26:49 -0400

    Create colored triangle with fixed function pipeline

---
+++ ep02-fixed-pipeline.py
@@ -0,0 +1,53 @@
+import glfw
+from OpenGL.GL import *
+import numpy as np
+
+def main():
+    # Initialize the library
+    if not glfw.init():
+        raise Exception("Can not be initalized!")
+    # Create a windowed mode window and its OpenGL context
+    window = glfw.create_window(640, 480, "Hello World", None, None)
+    if not window:
+        glfw.terminate()
+        return
+
+    # Make the window's context current
+    glfw.make_context_current(window)
+
+    vertices = [-0.5, -0.5, 0.0,
+                0.5, -0.5, 0.0,
+                0.0, 0.5, 1.0]
+
+    colors = [1.5, 0.0, 0.0,
+              0.0, 1.0, 0.0,
+              0.0, 0.0, 1.0]
+
+    vertices = np.array(vertices, dtype=np.float32)
+    colors = np.array(colors, dtype=np.float32)
+
+    glEnableClientState(GL_VERTEX_ARRAY)
+    glVertexPointer(3, GL_FLOAT, 0, vertices)
+
+    glEnableClientState(GL_COLOR_ARRAY)
+    glColorPointer(3, GL_FLOAT, 0, colors)
+
+    glClearColor(0, 0.1, 0.1, 1)
+
+    # Loop until the user closes the window
+    while not glfw.window_should_close(window):
+        # Render here, e.g. using pyOpenGL
+        glClear(GL_COLOR_BUFFER_BIT)
+
+        glDrawArrays(GL_TRIANGLES, 0, 3)
+        # Swap front and back buffers
+        glfw.swap_buffers(window)
+
+        # Poll for and process events
+        glfw.poll_events()
+
+    glfw.terminate()
+
+
+if __name__ == "__main__":
+    main()
